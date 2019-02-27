d5ac9275f6e1c74ee38ea487a0113beecbf12eaa
Sanjith K <sanjith_k@photonint.com>
2019-02-27 15:05:21 +0530

    remove duplicate element from array

---
+++ callback/duplicateArr.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  var a = [10,20,30,40];
+  var b = [20,10,50,100,10];
+  var c = [...a,...b];
+  function removeDuplicates(arr) {
+      var i = 0, len=arr.length, ansArray=[], finalObj={}; 
+      while (i<len) {
+          finalObj[arr[i]]=0;
+          i++;
+      }
+      for (val in finalObj) {
+          ansArray.push(parseInt(val));
+      }
+      return ansArray;
+  }
+  var result = removeDuplicates(c);
+  res.end(result.toString());
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
