eef1f6f08ec97ce5fd5d2c6744751bf10d53c9be
sanjithk7 <sanjith93k7@gmail.com>
2019-03-08 14:19:27 +0530

    committing files.

---
+++ http-server/lodash.js
@@ -0,0 +1,2 @@
+const _ = require('lodash');
+console.log(_.fill([1,2,3,4,5],'sanjai', 1, 3));
--- http-server/staticfile.js
+++ http-server/staticfile.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 var server = http.createServer((req, res) => {
-    var readStream = fs.createReadStream('./static/index.html');
-    
+    var readStream = fs.createReadStream('./static/example.json');
+    res.writeHead(200, {'Content-type' : 'application/json' });
+    readStream.pipe(res);
 });
 server.listen(3000);
